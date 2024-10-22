e0a497523a6100cc32bdafc990c244a1d36b68b2
Bhavya-sn <bhavyasnikiehbsn@gmail.com>
2024-10-22 15:44:13 +0530

    reload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request, url_for,flash,redirect,jsonify,send_from_directory
 import requests
+from datetime import datetime
 
 
 
@@ -17,19 +18,54 @@ def display_sensor_data():
     fastapi_url = "http://127.0.0.1:8000/getdata"  
     response = requests.get(fastapi_url, params={})
 
-    
     if response.status_code == 200:
         data = response.json().get('data')
     else:
         data = []
 
- 
+    print(data)
     return render_template('table.html', data=data)   
+
+@app.route('/getdata',methods=['GET'])
+def display_sensor_data_withDate():
+
+    selected_date = request.args.get('date')
+    # date_part, time_part = selected_date.split('T')
+    # formatted_date = date_part.replace('-', '/')
+    
+    fastapi_url = "http://127.0.0.1:8000/getdata"  
+    response = requests.get(fastapi_url, params={"date": selected_date})
+    print(selected_date)
+
+    
+    if response.status_code == 200:
+        data = response.json().get('data')
+    else:
+        data = []
+    print(data)
+    return render_template('reload.html', data=data)
+
+
+
+
+# @app.route('/getdata')
+# def display_sensor_data():
+    
+#     fastapi_url = "http://127.0.0.1:8000/getdata"  
+#     response = requests.get(fastapi_url, params={})
+
+    
+#     if response.status_code == 200:
+#         data = response.json().get('data')
+#     else:
+#         data = []
+
+ 
+#     return render_template('table.html', data=data)  
     
 # @app.route('/')
 # def display_sensor_data():
 #     return render_template('table.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)     
